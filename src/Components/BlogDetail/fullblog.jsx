f214a0efe985a5205d3df7d480e9ad18e62468f8
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-03-05 09:59:37 +0530

    Get the detailed blog on the website

--- src/Components/BlogDetail/fullblog.jsx
+++ src/Components/BlogDetail/fullblog.jsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import axios from "axios"
 import loadingGif from '../../assets/loading.svg'
+import commentImage from "/comment.svg";
+import style from "./fullblog.module.sass";
+import Navbar from "../Nav";
+
 
 export default function FullBlog() {
-    const [loading , setLoading] = useState(true)
-    const [blog , setBlog] = useState({})
-    const {id} = useParams()
-    useEffect(()=> {
-        async function FetchBlog(){
+    const [loading, setLoading] = useState(true)
+    const [blog, setBlog] = useState({})
+    const { id } = useParams()
+    useEffect(() => {
+        async function FetchBlog() {
             try {
                 const data = await axios.get(`${import.meta.env.VITE_URL}/blogs/${id}`).then(res => res.data)
                 setBlog(data.blog)
@@ -16,13 +20,45 @@ export default function FullBlog() {
             } catch (error) {
                 throw new Error(error)
             }
-        
         }
         FetchBlog()
-    } , [])
+    }, [])
     return (
         <>
-
+            {
+                loading ?
+                    <div className='loading'>
+                        <img src={loadingGif} alt='Loading..'></img>
+                    </div>
+                    :
+                    <>
+                        <Navbar></Navbar>
+                        <main className={style.main}>
+                            <div className={style.blogInf}>
+                                <h4>{blog.timeStamp}</h4>
+                                <h4>Written by {blog.user.userName}</h4>
+                            </div>
+                            <h1>{blog.title}</h1>
+                            <div className={style.blogContent}>
+                                <p>{blog.text}</p>
+                            </div>
+                            <div className={style.blogComments}>
+                                <h3>Comments</h3>
+                                {
+                                    blog.comments.length === 0 ? <h4>No comments..yet!</h4> :
+                                        blog.comments.map((comment, index) => {
+                                            return (
+                                                <div className="comment" key={index}>
+                                                    <h4>{comment.user.userName}</h4>
+                                                    <p>{comment.text}</p>
+                                                </div>
+                                            )
+                                        })
+                                }
+                            </div>
+                        </main>
+                    </>
+            }
         </>
     )
 }
