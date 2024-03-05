41b3794cd9aee64fb70eefd741cf001573ebdadd
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-03-05 16:14:45 +0530

    Allow users to sign in/up

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,20 +1,36 @@
+import axios from "axios";
 import Navbar from "./Nav";
 import styles from './login.module.sass'
+import { useState } from "react";
 
 export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post(`${import.meta.env.VITE_URL}/login`, { userName: username, password: password })
+            .then(res => {
+                localStorage.setItem('token', res.data.token)
+                window.location.href = '/'
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     return (
         <main className={styles.main}>
             <Navbar/>
             <div className={styles.login}>
                 <h1>Login</h1>
-                <form>
+                <form method="POST" onSubmit={handleSubmit}>
                     <div className="unContainer">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name="userName" required></input>
+                        <input type="text" id="username" name="userName" required minLength={1} value={username} onChange={e => setUsername(e.target.value)}></input>
                     </div>
                     <div className="pwContainer">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" required></input>
+                        <input type="password" id="password" name="password" required minLength={1} onChange={e => setPassword(e.target.value)} value={password}></input>
                     </div>
                     <button type="submit">Login</button>
                 </form>
--- src/Components/signup.jsx
+++ src/Components/signup.jsx
@@ -1,19 +1,34 @@
+import axios from 'axios';
 import styles from './login.module.sass'
 import Navbar from "./Nav";
+import { useState } from 'react';
 export default function Signup(){
+    const [username , setUsername] = useState('')
+    const [password , setPassword] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post(`${import.meta.env.VITE_URL}/signup`, {userName: username, password: password})
+            .then(res => {
+                localStorage.setItem('token', res.data.token)
+                window.location.href = '/'
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
     return (
         <main className={styles.main}>
             <Navbar/>
             <div className={styles.login}>
                 <h1>Signup</h1>
-                <form>
+                <form method='POST' onSubmit={handleSubmit}>
                     <div className="unContainer">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name="userName" required></input>
+                        <input type="text" id="username" name="userName" required minLength={1} value={username} onChange={(e) => setUsername(e.target.value)}></input>
                     </div>
                     <div className="pwContainer">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" required></input>
+                        <input type="password" id="password" name="password" required minLength={4} value={password} onChange={(e) => setPassword(e.target.value)}></input>
                     </div>
                     <button type="submit">Register</button>
                 </form>
